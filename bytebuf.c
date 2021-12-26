39d548ddd0a70e1162cfae4ff66c548eec7bacdb
Ashley <84289665+ashleeeeyy@users.noreply.github.com>
2021-12-26 16:25:20 -0500

    dynamically resizeable buffers

--- bytebuf.c
+++ bytebuf.c
@@ -4,9 +4,23 @@
 
 #include <printf.h>
 #include <arpa/inet.h>
+#include <libc.h>
 #include "bytebuf.h"
 
-int buffer_write_u_int_16(u_int16_t num, char buffer[], int *writehead){
+//scales the buffer if needed
+int scale_buffer(char buffer[], int *bufferzie, const int*writehead, int number_of_bytes_to_write){
+    //if we need to scale
+    if(*writehead+number_of_bytes_to_write > *bufferzie){
+        //move array to new buffer
+        char newbuffer[*bufferzie*=2];
+        memcpy(newbuffer, buffer, *writehead);
+        free(buffer);
+        return 1;
+    }
+    return 0;
+}
+int buffer_write_u_int_16(u_int16_t num, char buffer[], int *writehead, int *buffersize){
+    scale_buffer(buffer, buffersize, writehead, sizeof num);
     //set a int16 pointer starting at the writehead in the arrays memory
     u_int16_t *il = (u_int16_t *) (&buffer[*writehead]);
 
@@ -20,7 +34,9 @@ int buffer_write_u_int_16(u_int16_t num, char buffer[], int *writehead){
     return 0;
 }
 
-int buffer_write_u_int_8(u_int8_t num, char buffer[], int *writehead){
+int buffer_write_u_int_8(u_int8_t num, char buffer[], int *writehead, int *buffersize){
+    scale_buffer(buffer, buffersize, writehead, sizeof num);
+
     u_int8_t *il = (u_int8_t *) (&buffer[*writehead]);
     *il = num;
     *writehead = (int) (*writehead + sizeof num);
@@ -28,17 +44,17 @@ int buffer_write_u_int_8(u_int8_t num, char buffer[], int *writehead){
     return 0;
 }
 
-int buffer_write_char_array(char src[], int srclen, char buffer[], int *writehead){
+int buffer_write_char_array(char src[], int srclen, char buffer[], int *writehead, int *buffersize){
     int i;
     //write each byte of the source to the destination
     for (i = 0; i < srclen; ++i) {
-        buffer_write_u_int_8(src[i], buffer, writehead);
+        buffer_write_u_int_8(src[i], buffer, writehead, buffersize);
     }
 
     return 0;
 }
 
-int buffer_write_string(const char src[], char buffer[], int *writehead){
+int buffer_write_string(const char src[], char buffer[], int *writehead, int *buffersize){
     //copy pointer as to not modify i
     const char* string = src;
     //get the size
@@ -47,13 +63,13 @@ int buffer_write_string(const char src[], char buffer[], int *writehead){
         size++;
         ++string;
     }
-    buffer_write_u_int_8(size, buffer, writehead);
+    buffer_write_u_int_8(size, buffer, writehead, buffersize);
     //reset pointer
     string = src;
     //write the actual data
 
     while(*string != '\0'){
-        buffer_write_u_int_8(*string, buffer, writehead);
+        buffer_write_u_int_8(*string, buffer, writehead, buffersize);
         ++string;
     }
 
--- bytebuf.h
+++ bytebuf.h
@@ -4,14 +4,14 @@
 
 #include <arm/types.h>
 
-int buffer_write_u_int_16(u_int16_t num, char buffer[], int *writehead);
-int buffer_write_u_int_8(u_int8_t num, char buffer[], int *writehead);
+int buffer_write_u_int_16(u_int16_t num, char buffer[], int *writehead, int *buffersize);
+int buffer_write_u_int_8(u_int8_t num, char buffer[], int *writehead, int *buffersize);
 
 //write non terminated byte array of srclen to the buffer
-int buffer_write_char_array(char src[], int srclen, char buffer[], int *writehead);
+int buffer_write_char_array(char src[], int srclen, char buffer[], int *writehead, int *buffersize);
 
 //hexdump the duffer to stdin
 int debug_output_buffer(char buffer[], int size);
 
 //Write null terminated string to buffer
-int buffer_write_string(const char src[], char buffer[], int *writehead);
+int buffer_write_string(const char src[], char buffer[], int *writehead, int *buffersize);
